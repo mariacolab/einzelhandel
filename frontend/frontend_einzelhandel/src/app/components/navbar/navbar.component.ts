2bd6963af7d3c73a6f18dcca877d3367f6975de3
x <nanophilip@users.noreply.github.com>
2025-03-02 08:55:23 +0800

    fix: restore navbar

--- frontend/frontend_einzelhandel/src/app/components/navbar/navbar.component.ts
+++ frontend/frontend_einzelhandel/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-//import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
@@ -8,7 +8,7 @@ import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, CommonModule], //MatIconModule
+  imports: [MatToolbarModule, MatButtonModule, CommonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
